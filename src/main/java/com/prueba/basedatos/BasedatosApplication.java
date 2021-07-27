17e67bf0a51e27b77623c2de57a559c6e683eb84
Andres Castellanos <andresrobanesky@gmail.com>
2021-07-26 21:10:56 -0500

    version inicial proyecto

---
+++ src/main/java/com/prueba/basedatos/BasedatosApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.basedatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasedatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasedatosApplication.class, args);
+	}
+
+}
